a7ff4efe10c0f530165a10e802b0e2e4a5bce002
vitorlperez <vitorlperez1@gmail.com>
2021-02-03 09:06:42 -0400

    test att

--- test.py
+++ test.py
@@ -8,21 +8,13 @@ class TestHello(unittest.TestCase):
         app.app.testing = True
         self.app = app.app.test_client()
 
-    def test_hello(self):
+    def test_list(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
 
     def test_hello_hello(self):
-        rv = self.app.get('/hello/')
+        rv = self.app.get('/create/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
-
-    def test_hello_name(self):
-        name = 'Simon'
-        rv = self.app.get(f'/hello/{name}')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
 
 if __name__ == '__main__':
     unittest.main()
